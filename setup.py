d7cadcc1ec19a10da73902d69e10acd28a46f420
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2017-09-27 13:38:47 +1000

    bump 1.0.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ instructions.
 
 setup(
     name="django-prometheus",
-    version="1.0.9",
+    version="1.0.10",
     author="Uriel Corfa",
     author_email="uriel@corfa.fr",
     description=(
