46a94f1d37122bed979428095107be61d97eac18
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-06-22 16:44:56 +1000

    Use 3 digit version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ instructions.
 
 setup(
     name="django-prometheus",
-    version="1.0.10.1",
+    version="1.0.900",
     author="Uriel Corfa",
     author_email="uriel@corfa.fr",
     description=(
